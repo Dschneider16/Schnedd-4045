5bb1a80d707ab664695fa867ee70047f19351ecd
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-20 14:49:33 -0400

    Inital commit of spring boot

---
+++ src/main/java/com/AnimalFinder/AnimalFinderApplication.java
@@ -0,0 +1,13 @@
+package com.AnimalFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AnimalFinder/AnimalFinder/AnimalFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AnimalFinder.AnimalFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
